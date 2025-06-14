71df050d452a44c3c702c790e8e734dc321a3f6c
Tarun Julmi <julmetech@gamil.com>
2025-06-14 06:00:02 +0000

    bothe app.jsx and main are added

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
